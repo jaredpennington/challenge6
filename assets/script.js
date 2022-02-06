b9707eee0fa76035011af7f18fd7629c7af2ed4e
jaredpennington <94023319+jaredpennington@users.noreply.github.com>
2022-02-05 23:15:58 -0500

    added columns and elements

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ var container = document.getElementById("container");
 
 
 function oneCall(lat, lon) {
-  var currentWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${apiKey}`
+  var currentWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
 
   fetch(currentWeatherUrl)
     .then(function (response) {
@@ -15,18 +15,54 @@ function oneCall(lat, lon) {
     .then(function (data) {
       console.log(data);
 
-    for(var i = 0; i < 5; i++) {
+    for(var i = 1; i < 6; i++) {
         var day = new Date(data.daily[i].dt*1000).toLocaleDateString("en-US")
+        var icon = data.daily[i].weather[0].icon
+        var temp =  data.daily[i].temp.day
+        var wind = data.daily[i].wind_speed
+        var humidity = data.daily[i].humidity
+        createForcast(day,icon,temp,wind,humidity)
         console.log(day)
         var newDay = new Date
         //var date = 
-        var pElTemp = document.createElement("p")
-        pElTemp.textContent = "temp:" + data.daily[i].day
-        
     }
-    });
+});
 }
 
+
+
+
+function createForcast(day,icon,temp,wind,humidity) {
+    //we need date, icon for weather, temp, wind, humidity
+    var ElDay = document.createElement("p")
+    var ElIcon = document.createElement("img")
+    var ElTemp = document.createElement("p")
+    var ElWind = document.createElement("p")
+    var ElHumidity = document.createElement("p")
+//http://openweathermap.org/img/wn/10d@2x.png
+    ElDay.textContent = day
+    ElIcon.setAttribute("src",`http://openweathermap.org/img/wn/${icon}.png`) 
+    ElTemp.textContent = `temp:${temp}\u00B0F`
+    ElWind.textContent =  `wind:${wind}mph`
+    ElHumidity.textContent = `humidity:${humidity}%`
+
+var column = $("<div>")
+    .addClass("col-2")
+    .appendTo("container")
+
+var cardHeader = $("<h3>")
+    .addClass("card-header")
+    .html(`img src="${icon}"></img><p>${day}</p>`)
+    .appendTo(card)
+
+    var card = $("<div>")
+        .addClass("card")
+        .appendTo(container)
+
+
+    card.append(ElTemp,ElWind,ElHumidity)
+    
+}
 
 
 
